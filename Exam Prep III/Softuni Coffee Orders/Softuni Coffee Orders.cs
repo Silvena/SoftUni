825d20a908e22f77c71b0add2766fbb89231d838
Silvena Shtereva <silvenag@gmail.com>
2017-07-06 23:06:46 +0300

    exams

---
+++ Exam Prep III/Softuni Coffee Orders/Softuni Coffee Orders.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace softuniCoffeOrders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOrders = int.Parse(Console.ReadLine());
+            decimal totalExpenses = 0;
+            for (int i = 0; i < countOrders; i++)
+            {
+                decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
+
+                DateTime orderDate = DateTime.ParseExact(Console.ReadLine(), "d/M/yyyy", CultureInfo.InvariantCulture);
+
+                decimal countCapsules = decimal.Parse(Console.ReadLine());
+
+                int daysInMonth = DateTime.DaysInMonth(orderDate.Year, orderDate.Month);
+
+                decimal currentPrice = daysInMonth * countCapsules * pricePerCapsule;
+
+                totalExpenses += currentPrice;
+
+                Console.WriteLine($"The price for the coffee is: ${currentPrice:F2}");
+            }
+            Console.WriteLine($"Total: ${totalExpenses:F2}");
+
+
+        }
+    }
+
+}
