eaa3f5afc7134ee25119aea693e4ddcdf0225cac
Silvena Shtereva <silvenag@gmail.com>
2017-07-06 23:03:26 +0300

    Exam prep I

---
+++ Exam Prep I/Endurance Rally/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace Endurance_Rally
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var drivers = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            var track = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            var chekPoint = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            foreach (var driver in drivers)
+            {
+                double fuel = driver.First();
+
+                for (int i = 0; i < track.Length; i++)
+                {
+                    var currentZoneFuel = track[i];
+                    if (chekPoint.Contains(i))
+                    {
+                        //+
+                        fuel += currentZoneFuel;
+                    }
+                    else
+                    {
+                        //-
+                        fuel -= currentZoneFuel;
+                    }
+                    if (fuel <= 0)
+                    {
+                        Console.WriteLine($"{driver} - reached {i}");
+                        break;
+                    }
+
+                }
+
+                if (fuel > 0)
+                {
+                    Console.WriteLine($"{driver} - fuel left {fuel:F2}");
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Exam Prep I/SoftUni Karaoke/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Karaoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var participants = Console.ReadLine()
+                .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(p => p.Trim())
+                .ToArray();
+            var nameSong = Console.ReadLine()
+                .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(p => p.Trim())
+                .ToArray();
+
+            var result = new Dictionary<string, List<string>>();
+
+            string line = Console.ReadLine();
+            while (line != "dawn")
+            {
+                var performanse = line
+                .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(p => p.Trim())
+                .ToArray();
+                var participant = performanse[0];
+                var song = performanse[1];
+                var award = performanse[2];
+                if (participants.Contains(participant) && nameSong.Contains(song))
+                {
+                    if (!result.ContainsKey(participant))
+                    {
+                        result[participant] = new List<string>();
+                    }
+                    var awards = result[participant];
+                    if (!awards.Contains(award))
+                    {
+                        awards.Add(award);
+                    }
+                }
+                line = Console.ReadLine();
+            }
+            if (result.Any())  //result.count == 0
+            {
+                foreach (var kvp in result.OrderByDescending(p => p.Value.Count).ThenBy(p => p.Key))
+                {
+                    var participant = kvp.Key;
+                    var awards = kvp.Value;
+                    Console.WriteLine($"{participant}: {awards.Count} awards");
+                    foreach (var award in awards.OrderBy(a => a))
+                    {
+                        Console.WriteLine($"--{award}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No awards");
+            }
+        }
+    }
+}
